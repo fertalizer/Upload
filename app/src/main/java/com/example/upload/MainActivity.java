bef312ececd3a5f6924e9af76268e366831de2c0
fertalizer <fertalizer54321@gmail.com>
2019-01-27 15:37:22 +0800

    測試用push上傳程式

--- app/src/main/java/com/example/upload/MainActivity.java
+++ app/src/main/java/com/example/upload/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.upload;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    void testPush() {
+        Log.d("test","測試用push上傳程式")
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
